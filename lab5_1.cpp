1eb20f71f757fbc8c86b8deb32ad4242170d204d
Nunnalin Bingbing <61963924+snnmbb@users.noreply.github.com>
2020-12-15 16:43:56 +0700

    Add code

--- lab5_1.cpp
+++ lab5_1.cpp
@@ -5,27 +5,28 @@ int main()
 {
  	int even=0;
     int odd=0;
-    int num;
-    int n = 0;
-   
+    int num = 1;
     while(num!=0)
     { 
         cout << "Enter an integer: ";
         cin >> num;
-        if(num%2==0 && num != 0)
-        {
+        if(num%2==0 )
+		{
+			if(num !=0)
+        	{
             even++;
-        }
+        	}
+		}
+        
         else
         {
             odd++;
         }
     }
-        cout << "#Even numbers = ";	
-        cout << even;
-        cout << "#Odd numbers = ";	
-        cout << odd;
+    cout << "#Even numbers = " ;	
+    cout << even << "\n";
+    cout << "#Odd numbers = ";	
+    cout << odd;
 
-    
     return 0;
 }
